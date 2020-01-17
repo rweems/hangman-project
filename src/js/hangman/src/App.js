7a3b18dce909caa45e7080f1a822d6789b3976cb
Bao Nguyen <baovu88@yahoo.com>
2020-01-17 15:39:25 -0500

    working imgs

--- src/js/hangman/src/App.js
+++ src/js/hangman/src/App.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import Word from './Words/Word';
 import {getAllWords} from './client';
+import img1 from './img/hangman1.png';
+import img2 from './img/hangman2.png';
+import img3 from './img/hangman3.png';
+import img4 from './img/hangman4.png';
+import img5 from './img/hangman5.png';
+import img6 from './img/hangman6.png';
+import img7 from './img/hangman7.png';
 
 class App extends Component {
-
+  i = 0;
   state = {
+    // imgs: ['./img/hangman1.png',
+    //        './img/hangman2.png',
+    //        './img/hangman3.png',
+    //         './img/hangman4.png',
+    //         './img/hangman5.png',
+    //         './img/hangman6.png',
+    //         './img/hangman7.png'],
+    imgs: [img1,img2,img3,img4,img5,img6,img7],
+    img: img1,
     words: [],
     word: {
       name: 'myWord',
@@ -14,8 +30,14 @@ class App extends Component {
     }
   }
 
+  imgSwitch(){
+    this.i = (this.i+1)%this.state.imgs.length;
+    this.setState({img:this.state.imgs[this.i]});
+  }
+
   componentDidMount() {
     this.fetchWords();
+    this.imgSwitch();
   }
 
   fetchWords = () => {
@@ -34,6 +56,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hangman</h1>
+        <button onClick={()=>{this.imgSwitch()}}>click me</button>
+        <img src={this.state.img} alt="img" />
         {this.state.words.map(word => (
           <Word key={word.wordId} word={word} />
         ))}
---
+++ src/js/hangman/src/Hangman/HangMan.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HangMan.css';
+
+const word = (props) => {
+    return (
+        <div className='Word'>
+            <h3>{props.word.name}</h3>
+            <p>{props.word.definition}</p>
+            <p>Difficulty: {props.word.difficulty}</p>
+        </div>
+    );
+}
+
+export default word;
